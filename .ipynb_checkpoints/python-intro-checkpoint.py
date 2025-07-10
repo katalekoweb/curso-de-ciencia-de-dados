fb5ad3167aa5a569e4b6ab5341fd9e4cf326d586
Kataleko <katalekoweb@gmail.com>
2025-07-10 11:26:54 +0100

    first commit

---
+++ .ipynb_checkpoints/python-intro-checkpoint.py
@@ -0,0 +1,26 @@
+# criado no final dos anos 80 por Guido van Rossum
+# linguagem de programação de alto nível, interpretada, de tipagem dinâmica
+
+# Variavel - é um espaço na memória que armazena um valor
+# Tipos de dados primitivos
+# int - inteiro
+# float - ponto flutuante
+# str - string
+# bool - booleano
+# String
+
+# Declaracao implicita
+# Linguagem fracamente tipada
+# Linguagem sensitiva a maiusculas e minusculas
+
+x = 1 # int
+y = 3.13 # float
+name = "Kata" # String
+is_valid = True # bool
+is_valid = False # bool
+
+# Verificar o tipo de dado
+print(type(x)) # <class 'int'>
+print(type(y)) # <class 'float'>
+print(type(name)) # <class 'str'>
+print(type(is_valid)) # <class 'bool'>
---
+++ 6-46-referencias.py
@@ -0,0 +1,6 @@
+# referencias
+# python notes for professionals
+# https://books.goalkicker.com/PythonBook/
+
+# Python fluente
+# https://www.amazon.com.br/Python-Fluente-Conciso-Guia-Programadores/dp/8575229440
